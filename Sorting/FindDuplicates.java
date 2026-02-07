9bd31a6f195a03f42d52597f665db0a0fa2a34ca
Om Gupta <your_email@example.com>
2026-02-08 00:24:56 +0530

    Sorting Solved Problems

---
+++ Sorting/FindDuplicates.java
@@ -0,0 +1,24 @@
+public class FindDuplicates {
+    public int duplicates(int []nums){
+        int slow = nums[0];
+        int fast  =nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+    }
+        while(slow != fast);
+            slow = nums[0];
+            while(slow!=fast){
+                slow =nums[slow];
+                fast = nums[fast];
+            }
+            return slow;
+        }
+    
+    public static void main(String args[]){
+        FindDuplicates obj = new FindDuplicates();
+        int nums [] = {1,3,4,2,2};
+        System.out.println(obj.duplicates(nums));
+    }
+}
